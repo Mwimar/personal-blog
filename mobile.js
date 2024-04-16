441ebf745c21c7ca17a4d5e267b216832c3c4c2f
Mark Mwima <markmwimar@gmail.com>
2024-04-16 12:15:01 +0300

    updated mobile menu

---
+++ mobile.js
@@ -0,0 +1,8 @@
+const mobileMenuBtnElement = document.getElemtById("mobile-menu-btn");
+const mobileMenuElement = document.getElementById("mobile-menu");
+
+function toggleMobileMenu() {
+  mobileMenuElement.classList.toggle("open");
+}
+
+mobileMenuBtnElement.addEventListener("click", toggleMobileMenu);
